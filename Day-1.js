de7b6ab4e627572867fed060637dac271e0a19dc
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-03 07:00:21 +0530

    day2

--- Day-1.js
+++ Day-1.js
@@ -1,1 +1,33 @@
 // Variables and Datatypes
+
+// variables are used to store some data
+
+// datatypes -> primitive(in-built) and referrence data type
+// primitive / value datatype -> string, boolean, number, undefined, null
+
+// var fname = "vasanth";
+// var age = -28;
+// var amount = 100.5;
+// var isEligible = false;
+// var memoryNotCreated = undefined; // not recommended
+// var lastName = null;
+
+// console.log(fname, typeof fname);
+// console.log(age, typeof age);
+// console.log(amount, typeof amount);
+// console.log(isEligible, typeof isEligible);
+// console.log(memoryNotCreated, typeof memoryNotCreated);
+// console.log(lastName, typeof lastName);
+
+// let and const
+// var data; // declaration
+// var data = "dabdnabda"; // definition or initialisation
+// var data = "vasanth";
+
+// let data = "daadaa";
+// data = "dada"; // re-assign the data
+// console.log(data);
+
+// const PI = 22 / 7; // can be initialised but cannot be re assigned
+// PI = 10; // error
+// console.log(PI);
---
+++ Day-2.js
@@ -0,0 +1,44 @@
+// statically typed vs dynamically typed
+
+// Statically typed
+// int age = 10
+// age = 'vasanth' // error
+
+// dynamically typed language -> Js
+// let age = 10;
+// console.log(age);
+// age = "vasanth";
+// console.log(age);
+// age = true;
+// console.log(age);
+// age = undefined;
+// console.log(age);
+
+// operators
+// Arithmetic
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(10 / 3); // -> value will be in decimal format
+// console.log(10 % 3); // modulus -> remainder
+// console.log(3 ** 2); // exponential 10 ^ 2
+
+// Assignment operators
+// =
+// let a = 10;
+// let b = 15;
+// console.log(a + b);
+
+// let age = 20;
+// age += 15; // age = age + 15
+// console.log(age);
+// age -= 10; // age = age - 10
+// console.log(age);
+// age *= 5;
+// console.log(age);
+// age /= 2;
+// console.log(age);
+// age %= 2;
+// console.log(age);
+// age **= 2;
+// console.log(age);

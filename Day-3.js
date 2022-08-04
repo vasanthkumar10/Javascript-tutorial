b6bc2bd157f298a835ba1f6db6f0b97169c51f16
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-04 07:05:36 +0530

    04/08/22

---
+++ Day-3.js
@@ -0,0 +1,72 @@
+// comparison operators
+// let a = 10;
+// let b = 10;
+// console.log(a == b);
+
+// let a = 10;
+// let b = "10";
+// console.log(a == b); // == checks only value but not datatype
+// console.log(a === b); //  checks values and datatype
+
+// let a = 10;
+// let b = "10";
+// console.log(a != b);
+// console.log(a !== b);
+
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(5 >= 5);
+// console.log(10 <= 5);
+
+// ternary operator -> condtion ? true statement : false statement
+
+// 1 > 5 ? console.log("greater") : console.log("smaller");
+
+// string
+
+// let firstName = "Vasanthkumar";
+// let lastName = "Vinayagam";
+// // // Vasanthkumar Vinayagam
+// // console.log(firstName + " " + lastName);
+
+// // 'Vasanthkumar' is a good boy
+
+// // console.log("'" + firstName + "'" + " is a good boy");
+
+// // "Vasanthkumar" is a 'good' "boy"
+// // console.log('"' + firstName + '"' + " is a 'good' boy");
+
+// // string template literals -> multiple lines and easy concatenation
+// console.log(
+//   `"${firstName}' is a 'good' "boy". ${firstName}'s lastname is  ${lastName}`
+// );
+
+// let poem = `Oh god beautiful, oh god beautiful
+// at thy feet, oh god beautiful`;
+// console.log(poem);
+
+// weird javascript things
+// console.log(5 + 5); // 10
+// console.log(5 + "5"); // 55
+// console.log(5 + 5 + "5"); // 105
+// console.log("5" + 5); // 55
+// console.log("5" + 5 + 5); // 55
+// console.log("5" + "5"); // 55
+// console.log("ab" + 5); // ab5
+// console.log("ab" + "5"); // ab5
+
+// console.log(10 - 5); // 5
+// console.log("10" - 5); // 5
+// console.log(10 - "5"); // 5
+// console.log("10" - "5"); // 5
+// console.log("ab" - "5"); // NaN -> Not a Number
+// console.log("ab" - "bc"); // NaN -> Not a Number
+
+// practice
+// console.log("a" + "4");
+// console.log("4" + 4);
+// console.log("4" - 4);
+// console.log("10" - "6");
+// console.log("b" - "a");
+// console.log(2 + 4 - '10' + 10); // 6
+// console.log(2 + '4' + '-10' + 10); // 24-1010

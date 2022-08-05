1415d6270a5879433ea4a33af3cf6235b3a4a224
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-05 08:04:44 +0530

    day-4 05/08/22

---
+++ Day-4.js
@@ -0,0 +1,141 @@
+// conditional statements
+// if, if - else, else if ladder, switch
+
+// let age = 12;
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not Eligible to vote");
+// }
+
+// let num1 = 15;
+// let num2 = 10;
+// // greatest of 2 nums
+// if (num1 > num2) {
+//   console.log("num1 is greater");
+// } else {
+//   console.log("num2 is greater");
+// }
+
+// logical operators
+// && -> logical and
+// || -> logical or
+// ! -> logical not
+
+// let age = 13;
+// let isVoterIDAvailable = true;
+// if (age >= 18 && isVoterIDAvailable) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not Eligible to vote");
+// }
+
+// let isAadharAvailable = false;
+// let isVoterIDAvailable = false;
+// if (isAadharAvailable || isVoterIDAvailable) {
+//   console.log("Eligible to get passport");
+// } else {
+//   console.log("Not Eligible to get passport");
+// }
+
+// let num1 = 15;
+// let num2 = 10;
+// let num3 = 20;
+// greatest of 3 nums
+// if (num1 > num2) {
+//   if (num1 > num3) {
+//     console.log("num1 is greater");
+//   } else {
+//     console.log("num3 is greater");
+//   }
+// } else {
+//   if (num2 > num3) {
+//     console.log("num2 is greater");
+//   } else {
+//     console.log("num3 is greater");
+//   }
+// }
+
+// if (num1 > num2 && num1 > num3) {
+//   console.log("num1 is greater");
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log("num2 is greater");
+// } else {
+//   console.log("num3 is greater");
+// }
+
+// practice
+// if (10) {
+//   console.log("vasanth");
+// }
+
+// if (-10) {
+//   console.log("vasanth");
+// }
+
+// if (0) {
+//   console.log("vasanth");
+// }
+
+// if ("vasanth") {
+//   console.log("vasanth");
+// }
+
+// if ("") {
+//   console.log("vasanth");
+// }
+
+// if (undefined) {
+//   console.log("vasanth");
+// }
+
+// if (null) {
+//   console.log("vasanth");
+// }
+
+// if (NaN) {
+//   console.log("vasanth");
+// }
+
+// #############################################
+// falsy values
+// 0, false, '', undefined, null, NaN
+// #############################################
+
+// console.log(1 > 2 > 3); // false
+// console.log(3 > 2 > 1); // false
+
+// console.log(console.log("vasanth"));
+
+// short circuiting
+// let age = 20;
+// if (age > 18 && console.log("vasanth")) {
+//   console.log("eligible");
+// } else {
+//   console.log("not eligible");
+// }
+
+// let age = 20;
+// if (age > 18 || console.log("vasanth")) {
+//   console.log("eligible");
+// } else {
+//   console.log("not eligible");
+// }
+
+// if (console.log("vasanth")) {
+//   console.log("dabjkanfkaba");
+// }
+
+// logical Not !
+// true -> false
+// false -> true
+
+// if (!(10 < 5)) {
+//   console.log(10);
+// } else {
+//   console.log("5");
+// }
+
+// practice
+// console.log(!10, !"dadada"); // false
+// console.log(!null, !0, !""); // true

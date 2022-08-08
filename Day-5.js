887c4b3ca8ab38fb9b182351665fb814ba95b2e2
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-08 07:03:43 +0530

    day5 08/08/22

---
+++ Day-5.js
@@ -0,0 +1,79 @@
+// conditional statements
+
+// Switch case -> It is not recommended to use
+// number = 0;
+
+// if (number == 0) {
+//   console.log("Sunday");
+// } else if (number == 1) {
+//   console.log("monday");
+// } else if (number == 2) {
+//   console.log("tuesday");
+// } else if (number == 3) {
+//   console.log("wednesday");
+// } else if (number == 4) {
+//   console.log("thursday");
+// } else if (number == 5) {
+//   console.log("friday");
+// } else if (number == 6) {
+//   console.log("saturday");
+// } else {
+//   console.log("invalid ");
+// }
+
+// switch (number) {
+//   case 0:
+//     console.log("Sunday");
+//     break;
+//   case 10:
+//     console.log("Sunday duplicate");
+//     break;
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   default:
+//     console.log("Invalid data");
+//     break;
+// }
+
+// bitwise operators
+// bitwise AND -> &
+// console.log(5 & 3);
+// console.log(15 & 7);
+
+// bitwise OR -> |
+// console.log(5 | 3);
+// console.log(15 | 7);
+
+// bitwise NOT -> ~
+// console.log(~-5);
+// console.log(~15);
+
+// bitwise XOR ^
+// console.log(5 ^ 3);
+// console.log(15 ^ 7);
+
+// left shift <<
+// console.log(5 << 1);
+// console.log(7 << 2);
+// console.log(7 << 12);
+
+// // right shift >>
+// console.log(5 >> 1);
+// console.log(8 >> 2);
+// console.log(7 >> 4);

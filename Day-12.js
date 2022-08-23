f51c5c7120b3eb40743ec043e6a1eba5363b3571
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-23 07:05:18 +0530

    23/08/22 execution context

---
+++ Day-12.js
@@ -0,0 +1,30 @@
+// console.log(fname);
+
+// var fname = "vasanth";
+
+// console.log(fname);
+
+// add();
+// // sub();
+// function add() {
+//   console.log("adding...");
+// }
+
+// let sub = function () {
+//   console.log("subtracting...");
+// };
+
+// sub();
+
+let fname = "vasanth";
+
+function add() {
+  console.log("logging....");
+  let sub = function () {
+    console.log("sub....");
+  };
+
+  sub();
+}
+console.log(fname);
+console.log(add());

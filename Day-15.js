38f1d0fcc6671335926c594aaa78ee614c82f831
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-27 15:40:37 +0530

    27/08/22 part 2

--- Day-15.js
+++ Day-15.js
@@ -47,15 +47,194 @@
  *-------------------------
  */
 
-const p = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve("success");
-    // reject("error in promise");
-  }, 3000);
-});
+// const p = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve("success");
+//     // reject("error in promise");
+//   }, 3000);
+// });
 
-p.then(function (result) {
-  console.log(result);
-}).catch(function (error) {
-  console.log(error);
-});
+// p.then(function (result) {
+//   console.log(result);
+// }).catch(function (error) {
+//   console.log(error);
+// });
+
+/**
+ * Get commits using promises
+ */
+
+// function getUser(id) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       console.log(`Reading data from database with ${id}...`);
+//       let userName = "vasanth";
+//       resolve(userName);
+//       // reject("error in user");
+//     }, 1000);
+//   });
+// }
+
+// function getRepositories(userName) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       console.log(`getting repos of user ${userName}`);
+//       let repos = ["calcultor", "portfolio"];
+//       // resolve(repos);
+//       reject("error in repos");
+//     }, 2000);
+//   });
+// }
+
+// function getCommits(repo) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       console.log(`getting commits of repo ${repo}`);
+//       let commits = ["initial commit", "first commit"];
+//       resolve(commits);
+//     }, 2000);
+//   });
+// }
+
+// getUser(10)
+//   .then(function (userName) {
+//     console.log(userName);
+//     getRepositories(userName)
+//       .then(function (repos) {
+//         console.log(repos);
+//         getCommits(repos[0])
+//           .then(function (commits) {
+//             console.log(commits);
+//           })
+//           .catch(function (error) {
+//             console.log(error);
+//           });
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// getUser(10)
+//   .then(function (userName) {
+//     console.log(userName);
+//     return getRepositories(userName);
+//   })
+//   .then(function (repos) {
+//     console.log(repos);
+//     return getCommits(repos[0]);
+//   })
+//   .then(function (commits) {
+//     console.log(commits);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// const p1 = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve("p1 error");
+//   }, 2000);
+// });
+
+// const p2 = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve("p2");
+//   }, 1000);
+// });
+
+// p1.then(function (res) {
+//   console.log(res);
+// })
+//   .catch(function (err) {
+//     console.log(err);
+//   })
+//   .finally(function () {
+//     console.log("finally");
+//   });
+
+// Promise.all([p1, p2])
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// Promise.race([p1, p2])
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+/**
+ * --------------------
+ * ASYNC AWAIT
+ * --------------------
+ */
+
+function getUser(id) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log(`Reading data from database with ${id}...`);
+      let userName = "vasanth";
+      resolve(userName);
+      // reject("error in user");
+    }, 1000);
+  });
+}
+
+function getRepositories(userName) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log(`getting repos of user ${userName}`);
+      let repos = ["calcultor", "portfolio"];
+      resolve(repos);
+      // reject("error in repos");
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log(`getting commits of repo ${repo}`);
+      let commits = ["initial commit", "first commit"];
+      resolve(commits);
+    }, 2000);
+  });
+}
+
+// syntactic sugar of promise
+
+async function gitHubAction() {
+  try {
+    const userName = await getUser(10);
+    console.log(userName);
+    const repos = await getRepositories(userName);
+    console.log(repos);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("finally");
+  }
+}
+
+gitHubAction();
+
+// steps ->
+// Enter the hotel
+// go the seat
+// order food
+// prepare food
+// serve food
+// eat food
+// pay the bill
+// walk away
